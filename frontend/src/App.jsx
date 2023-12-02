c022a1e2ff14d0b2a2f7dc955627aa3d0b9bbe20
Andrey Mayuk <mayukda@gmail.com>
2023-12-02 19:27:15 +1000

    Configure routing for first web pages
    Update frontend/src/App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,41 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import logo from './logo.svg';
 import './App.css';
+import ChatsPage from './components/ChatsPage';
+import NotFoundPage from './components/NotFoundPage';
+import LoginPage from './components/LoginPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="*" element={<NotFoundPage />} />
+      <Route path="/" element={<ChatsPage />} />
+      <Route path="/login" element={<LoginPage />} />
+    </Routes>
+  </BrowserRouter>
+);
+
+// const App = () => (
+//   <div className="App">
+//     <header className="App-header">
+//       <img src={logo} className="App-logo" alt="logo" />
+//       <p>
+//         Edit
+//         {' '}
+//         <code>src/App.js</code>
+//         {' '}
+//         and save to reload.
+//       </p>
+//       <a
+//         className="App-link"
+//         href="https://reactjs.org"
+//         target="_blank"
+//         rel="noopener noreferrer"
+//       >
+//         Learn React
+//       </a>
+//     </header>
+//   </div>
+// );
 
 export default App;
