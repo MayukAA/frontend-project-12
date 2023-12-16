ba8aa47bfa959ff3ae433033d3ad286e55fda3df
Andrey Mayuk <mayukda@gmail.com>
2023-12-16 23:20:54 +1000

    Redirect to 'LoginPage' during deauthorization
    Update frontend/src/components/Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,14 +1,26 @@
 import { useContext } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import AuthorizationContext from '../context/AuthorizationContext';
+import routes from '../routes';
 
 const Navbar = () => {
   const { user, deAuthorization } = useContext(AuthorizationContext);
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    deAuthorization();
+
+    if (location.pathname === routes.chatsPagePath()) {
+      navigate(routes.loginPagePath());
+    }
+  };
 
   return (
     <nav className="shadow-sm navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container">
         <a className="navbar-brand" href="/">Hexlet Chat</a>
-        {!!user && <button type="button" className="btn btn-primary" onClick={deAuthorization}>Выйти</button>}
+        {!!user && <button type="button" className="btn btn-primary" onClick={handleClick}>Выйти</button>}
       </div>
     </nav>
   );
